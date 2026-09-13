b9fe4bbccf748cdd34975f9d7c9a03689557af73
only4matcha-cmd <only4matcha@gmail.com>
2026-09-13 21:18:41 +0800

    firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCmf_eSwBYbCFCAh_7gwdkDUjFLZhGHF7A",
+  authDomain: "tugas-xpm.firebaseapp.com",
+  projectId: "tugas-xpm",
+  storageBucket: "tugas-xpm.firebasestorage.app",
+  messagingSenderId: "1092797132447",
+  appId: "1:1092797132447:web:066b764fc219dea2223caa",
+  measurementId: "G-NBEB4F1PWW"
+});
+
+const messaging = firebase.messaging();
