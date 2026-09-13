270d2e6b445246b3f8e01a2557cee01ba84ea90f
only4matcha-cmd <only4matcha@gmail.com>
2026-09-13 22:01:05 +0800

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,14 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCnf_e5WBYbCFCAh_7gwdkDU",
+  apiKey: "AIzaSyCmf_eSwBYbCFCAh_7gwdkDUjFLZhGHF7A",
   authDomain: "tugas-xpm.firebaseapp.com",
   projectId: "tugas-xpm",
-  storageBucket: "tugas-xpm.appspot.com",
+  storageBucket: "tugas-xpm.firebasestorage.app",
   messagingSenderId: "1092797132447",
-  appId: "1:1092797132447:web:O66b764fc219dea2223caa"
+  appId: "1:1092797132447:web:066b764fc219dea2223caa",
+  measurementId: "G-NBEB4F1PWW"
 });
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.icon
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
