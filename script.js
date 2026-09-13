3c79a38bc171a927f8c2cb272bde2a8c2e9b2790
only4matcha-cmd <only4matcha@gmail.com>
2026-09-13 23:43:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ let isAdmin = false;
 adminSection.style.display = "none";
 
 let currentAdminPassword = localStorage.getItem("adminPassword") || "xpm123";
-const NOMOR_ADMIN_WA = "6281234567890"; // <-- Ganti dengan nomor WhatsApp-mu
+const NOMOR_ADMIN_WA = "6287745656583"; // <-- Ganti dengan nomor WhatsApp-mu
 
 if (localStorage.getItem("darkMode") === "enabled") {
     document.body.classList.add("dark-theme");
